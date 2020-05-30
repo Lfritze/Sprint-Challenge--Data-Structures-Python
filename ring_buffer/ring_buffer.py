8e052b2362eb06206e307b4e25da83ed9b7ddb3e
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-05-30 12:22:12 -0400

    ring_buffer tests are passing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,12 @@ class RingBuffer:
         self.oldest_element = 0
 
     def append(self, item):
-        self.store.[self.oldest_element] = item
+        self.store[self.oldest_element] = item
+
+        if self.oldest_element == self.capacity - 1:
+            self.oldest_element = 0
+        else:
+            self.oldest_element += 1
 
     def get(self):
-        pass
+        return [item for item in self.store if item is not None]
